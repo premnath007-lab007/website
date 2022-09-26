edacfb6d3b22c4eca35c5c185a90991ac806c97b
premnath007-lab007 <85789895+premnath007-lab007@users.noreply.github.com>
2022-09-26 14:41:08 +0530

    website

---
+++ src/data/BlogData.js
@@ -0,0 +1,22 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Resume",
+        tags:["Design"],
+        date:"Latest Updated",
+        imgSrc:"",
+        link:""
+    },
+    {
+        id:2,
+        name:"Resume",
+        tags:["Software developer"],
+        date:"Latest Updated",
+        imgSrc:"",
+        link:""
+    },
+   
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Jurrasic Park Game",
+        description:"Developed a game where the user has to jump over the dinosaur without hitting it used HTML, CSS, JavaScript,Nodejs,MongoDB to develop this game. We have used Heroku to host this game.",
+        tags:["Nodejs","MongoDB" ],
+        
+        demo:"https://jurrasicgame.herokuapp.com/",
+        github:"https://github.com/sridharp45/JurrasicWorld-WebGame"
+    },
+    {
+        id:2,
+        name:"Sorting Visualizer",
+        description:"Built Sorting visualizer using HTML, CSS, and JavaScript where users can select the type of sort algorithms, size of the array size, and speed of the visualization.",
+        tags:["react","JS"],
+        
+        // demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+        // github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"Covid app",
+        description:"Built UI screens for Covid app which provides data of vaccinations and deaths due to covid.",
+        tags:["UI/UX"],
+        
+        demo:"https://www.behance.net/gallery/126794743/Dose%28covid-vaccine-application%29",
+        // github:"https://github.com/codebucks27/devto-clone"
+    },
+    {
+        id:4,
+        name:"Plants Buying App",
+        description:"A cool application of plant store integrated with animations for user experience",
+        tags:["UI/UX"],
+        
+        demo:"https://react-sidebar.vercel.app/",
+        // github:"https://github.com/codebucks27/react-sidebar"
+       
+    },{
+        id:5,
+        name:"Whatsapp clone",
+        description:"Clone of a widely used app 'Whatsapp' with some major changes that gives users a immense experience through out all screens." ,
+        tags:["UI/UX"],
+        
+        demo:"https://www.behance.net/gallery/126872133/WhatsApp-application-redesign",
+        // github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    },
+    {
+        id:6,
+        name:"Car Animation",
+        description:"An blender project based on a action glimpse of car animation.",
+        tags:["Blender"],
+        
+        demo:"https://www.behance.net/gallery/125392739/car-annimation%283d-objects%29",
+        // github:"https://github.com/codebucks27/React-responsive-navbar"
+        
+    },
+    
+  
+] 
